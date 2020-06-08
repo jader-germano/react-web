b8dff765514000febf4a00eafbacf6075173197b
jader.germano <jader.germano@icloud.com>
2020-06-07 23:13:32 -0300

    Criação de funcionalide de upload de imagens,

--- src/pages/CollectPoint/collect-point.tsx
+++ src/pages/CollectPoint/collect-point.tsx
@@ -2,9 +2,12 @@ import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, Marker, TileLayer } from 'react-leaflet';
-import api from "../../services/api";
 import axios from 'axios';
 import { LeafletMouseEvent } from 'leaflet'
+import api from "../../services/api";
+
+import Dropzone from "../../Components/Dropzone/dropzone";
+
 import './styles.css'
 
 import logo from '../../assets/logo.svg'
@@ -34,19 +37,24 @@ interface IBGECityResponse {
 }
 
 const CollectPoint = () => {
-    const [ items, setItems ] = useState<Item[]>([]);
-    const [ ufs, setUfs ] = useState<string[]>([]);
-    const [ cities, setCities ] = useState<string[]>([]);
+    const [items, setItems] = useState<Item[]>([]);
+    const [ufs, setUfs] = useState<string[]>([]);
+    const [cities, setCities] = useState<string[]>([]);
 
-    const [ initialPosition, setInitialPosition ] = useState<[ number, number ]>([ 0, 0 ]);
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
 
-    const [ formData, setFormData ]
-        = useState({ name: '', email: '', whatsapp: '' });
+    const [formData, setFormData]
+        = useState({
+        name: '',
+        email: '',
+        whatsapp: ''
+    });
 
-    const [ selectedUF, setSelectedUF ] = useState('0');
-    const [ selectedCity, setSelectedCity ] = useState('0');
-    const [ selectedPosition, setSelectedPosition ] = useState<[ number, number ]>([ 0, 0 ]);
-    const [ selectedItems, setSelectedItems ] = useState<number[]>([]);
+    const [selectedUF, setSelectedUF] = useState('0');
+    const [selectedCity, setSelectedCity] = useState('0');
+    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const history = useHistory();
 
@@ -54,8 +62,8 @@ const CollectPoint = () => {
         navigator.geolocation.getCurrentPosition(position => {
             const { latitude, longitude } = position.coords;
 
-            setInitialPosition([ latitude, longitude ]);
-            setSelectedPosition([ latitude, longitude ]);
+            setInitialPosition([latitude, longitude]);
+            setSelectedPosition([latitude, longitude]);
         });
     })
 
@@ -69,7 +77,6 @@ const CollectPoint = () => {
         axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
             .then(response => {
                 const ufInitials = response.data.map(uf => uf.sigla);
-                console.log(ufInitials);
                 setUfs(ufInitials);
             });
     }, []);
@@ -78,12 +85,12 @@ const CollectPoint = () => {
         if (selectedUF === '0') {
             return;
         }
-        axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ selectedUF }/municipios`)
+        axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUF}/municipios`)
             .then(response => {
                 const cityNames = response.data.map(city => city.nome);
                 setCities(cityNames);
             });
-    }, [ selectedUF ]);
+    }, [selectedUF]);
 
     function handleSelectedUf(event: ChangeEvent<HTMLSelectElement>) {
         setSelectedUF(event.target.value);
@@ -94,12 +101,15 @@ const CollectPoint = () => {
     }
 
     function handleMapClick(event: LeafletMouseEvent) {
-        setSelectedPosition([ event.latlng.lat, event.latlng.lng ]);
+        setSelectedPosition([event.latlng.lat, event.latlng.lng]);
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
         const { name, value } = event.target;
-        setFormData({ ...formData, [name]: value })
+        setFormData({
+            ...formData,
+            [name]: value
+        })
     }
 
     function handleSelectItem(id: number) {
@@ -108,7 +118,7 @@ const CollectPoint = () => {
             const filteredItems = selectedItems.filter(item => item !== id);
             setSelectedItems(filteredItems);
         } else {
-            setSelectedItems([ ...selectedItems, id ]);
+            setSelectedItems([...selectedItems, id]);
         }
 
     }
@@ -116,15 +126,24 @@ const CollectPoint = () => {
     async function handleSubmit(event: FormEvent) {
         // Evitar reload da DOM on submit
         event.preventDefault();
+
         const { name, email, whatsapp } = formData;
         const uf = selectedUF;
         const city = selectedCity;
-        const [ latitude, longitude ] = selectedPosition;
+        const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name, email, whatsapp, uf, city, latitude, longitude, items
-        };
+        const data = new FormData()
+           data.append('name',name);
+           data.append('email',email);
+           data.append('whatsapp',whatsapp);
+           data.append('uf',uf);
+           data.append('city',city);
+           data.append('latitude', String(latitude));
+           data.append('longitude',String(longitude));
+           data.append('items',items.join(','));
+           selectedFile && data.append('image', selectedFile);
+
         console.log(data);
         await api.post('collect-point', data);
 
@@ -136,15 +155,17 @@ const CollectPoint = () => {
     return (
         <div id="page-create-collect-point">
             <header>
-                <img src={ logo } alt="Ecoleta"/>
+                <img src={logo} alt="Ecoleta"/>
 
                 <Link to="/">
                     <FiArrowLeft/>
                     Voltar para home
                 </Link>
             </header>
-            <form onSubmit={ handleSubmit }>
+            <form onSubmit={handleSubmit}>
                 <h1>Cadastro do<br/> Ponto de Coleta</h1>
+
+                <Dropzone onFileUploaded={setSelectedFile}/>
 
                 <fieldset>
                     <legend>
@@ -156,7 +177,7 @@ const CollectPoint = () => {
                             type="text"
                             name="name"
                             id="name"
-                            onChange={ handleInputChange }
+                            onChange={handleInputChange}
                         />
                     </div>
                 </fieldset>
@@ -168,7 +189,7 @@ const CollectPoint = () => {
                             type="email"
                             name="email"
                             id="email"
-                            onChange={ handleInputChange }
+                            onChange={handleInputChange}
                         />
                     </div>
                     <div className="field">
@@ -177,7 +198,7 @@ const CollectPoint = () => {
                             type="text"
                             name="whatsapp"
                             id="whatsapp"
-                            onChange={ handleInputChange }
+                            onChange={handleInputChange}
                         />
                     </div>
                 </div>
@@ -186,12 +207,12 @@ const CollectPoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
-                    <Map center={ initialPosition } zoom={ 15 } onClick={ handleMapClick }>
+                    <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
                         <TileLayer
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
-                        <Marker position={ selectedPosition }/>
+                        <Marker position={selectedPosition}/>
                     </Map>
                     <div className="field-group">
                         <div className="field">
@@ -199,12 +220,12 @@ const CollectPoint = () => {
                             <select
                                 name="uf"
                                 id="uf"
-                                value={ selectedUF }
-                                onChange={ handleSelectedUf }>
+                                value={selectedUF}
+                                onChange={handleSelectedUf}>
                                 <option value="0">Selecione uma UF</option>
-                                { ufs.map(uf => (
-                                    <option key={ uf } value={ uf }>{ uf }</option>
-                                )) }
+                                {ufs.map(uf => (
+                                    <option key={uf} value={uf}>{uf}</option>
+                                ))}
                             </select>
                         </div>
                         <div className="field">
@@ -212,12 +233,12 @@ const CollectPoint = () => {
                             <select
                                 name="city"
                                 id="city"
-                                value={ selectedCity }
-                                onChange={ handleSelectedUCity }>
+                                value={selectedCity}
+                                onChange={handleSelectedUCity}>
                                 <option value="0">Selecione uma Cidade</option>
-                                { cities.map(city => (
-                                    <option key={ city } value={ city }>{ city }</option>
-                                )) }
+                                {cities.map(city => (
+                                    <option key={city} value={city}>{city}</option>
+                                ))}
                             </select>
                         </div>
                     </div>
@@ -228,16 +249,16 @@ const CollectPoint = () => {
                         <span>Selecione um ou mais ítens abaixo</span>
                     </legend>
                     <ul className="items-grid">
-                        { items.map(item => (
+                        {items.map(item => (
                             <li
-                                key={ item.id }
-                                onClick={ () => handleSelectItem(item.id) }
-                                className={ selectedItems.includes(item.id) ? 'selected' : '' }
+                                key={item.id}
+                                onClick={() => handleSelectItem(item.id)}
+                                className={selectedItems.includes(item.id) ? 'selected' : ''}
                             >
-                                <img src={ item.image_url } alt={ item.title }/>
-                                <span>{ item.title } </span>
+                                <img src={item.image_url} alt={item.title}/>
+                                <span>{item.title} </span>
                             </li>
-                        )) }
+                        ))}
                     </ul>
                 </fieldset>
                 <button type="submit">
